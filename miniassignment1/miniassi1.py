eb3576fc8713bf1452fd854b4fea71e5b825e08d
nampekumar <nampekumar@deloitte.com>
2022-05-18 10:21:09 +0530

    first commit

--- miniassignment1/miniassi1.py
+++ miniassignment1/miniassi1.py
@@ -27,3 +27,44 @@ class SearchCommonElements(StringClass):
     def commonElements(self):
         removed = list(set(self.str1) & set(self.str2))
         return removed
+
+class EqualSumPairs():
+    def count(self, list1):
+        list2 = []
+        for i in list1:
+            sum = 0
+            for j in i:
+                sum += int(j)
+            list2.append(sum)
+        print("List of unique Sums in possible pairs of string: ")
+        print(set(list2))
+        return len(set(list2))
+
+
+
+value1 = input("Enter String1: ")
+obj1 = StringClass(value1)
+print("Length of the String: ")
+print(obj1.length())
+print("List of String: ")
+print(obj1.tolist(value1))
+obj2 = PairsPossible(value1)
+list1=obj2.pairs()
+print("Possible Pairs of String1 are: ")
+print(list1)
+print("-------------------------------------------------------------")
+
+value2 = input("Enter String2: ")
+obj3 = PairsPossible(value2)
+list2=obj3.pairs()
+print("Possible Pairs of String 2 are: ")
+print(list2)
+print("-------------------------------------------------------------")
+
+obj4=SearchCommonElements(value1,value2)
+print("Common Elements between String1 and string2: ")
+print(obj4.commonElements())
+obj5 = EqualSumPairs()
+length=obj5.count(list2)
+print("Count of unique sums in possible pairs of string2: ")
+print(length)
