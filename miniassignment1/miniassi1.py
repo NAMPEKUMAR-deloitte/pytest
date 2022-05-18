c7cf7f0da3fbac79d3b125fbc064f01677f8e6cd
nampekumar <nampekumar@deloitte.com>
2022-05-18 10:19:19 +0530

    first commit

--- miniassignment1/miniassi1.py
+++ miniassignment1/miniassi1.py
@@ -11,3 +11,19 @@ class StringClass:
     def tolist(self, value):
         list1 = list(value)
         return list1
+
+class PairsPossible(StringClass):
+    def __init__(self,value):
+        self.str = value
+    def pairs(self):
+        pair = list(combinations(self.str, 2))
+        return pair
+
+class SearchCommonElements(StringClass):
+    def __init__(self,str1,str2):
+        self.str1 = str1
+        self.str2 = str2
+
+    def commonElements(self):
+        removed = list(set(self.str1) & set(self.str2))
+        return removed
