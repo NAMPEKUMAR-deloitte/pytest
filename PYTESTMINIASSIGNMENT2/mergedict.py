5faf1ee9d354f75ffce21712e656b6cbaac17e46
nampekumar <nampekumar@deloitte.com>
2022-05-19 19:30:41 +0530

    ass2

---
+++ PYTESTMINIASSIGNMENT2/mergedict.py
@@ -0,0 +1,10 @@
+print("Merge following two Python dictionaries into one: ")
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+for i in dict2:
+    if i in dict1:
+        dict1[i]=dict2[i]
+    else:
+        dict1[i]=dict2[i]
+print(dict1)
+print()
