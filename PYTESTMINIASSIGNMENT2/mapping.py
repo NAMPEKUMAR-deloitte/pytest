85df1bcacc9ee34b5a261c098ab96e2583602be0
nampekumar <nampekumar@deloitte.com>
2022-05-19 19:27:21 +0530

    ass2

---
+++ PYTESTMINIASSIGNMENT2/mapping.py
@@ -0,0 +1,8 @@
+print("Map the dictionary in the following manner: ")
+Keys = ["Ten", "Twenty", "Thirty"]
+values=[10, 20, 30]
+dictinoary={}
+for key in range(len(Keys)):
+    dictinoary[Keys[key]]=values[key]
+print(dictinoary)
+
